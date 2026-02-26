6590a9c096c7d3a69334a42815788f1237aceacd
Alexander <alexanderklush8@gmail.com>
2026-02-26 23:50:53 +0300

    feat: add grayscale option to OneBit Quantizer

--- Dither/Quantizers/OneBitQuantizer.cs
+++ Dither/Quantizers/OneBitQuantizer.cs
@@ -2,11 +2,30 @@ namespace Dither.Quantizers;
 
 public class OneBitQuantizer : IQuantizer
 {
+    private bool IsGrayscale { get; set; }
+    
+    public OneBitQuantizer(bool isGrayscale)
+    {
+        IsGrayscale = isGrayscale;
+    }
+    
     public float[] Quantize(float[] pixels)
     {
-        for (var i = 0; i < pixels.Length; i++)
+        if (IsGrayscale)
         {
-            pixels[i] = pixels[i] < 128 ? 0 : 255;
+            var average = pixels.Average();
+            
+            for (var i = 0; i < pixels.Length; i++)
+            {
+                pixels[i] = average < 128 ? 0 : 255;
+            }
+        }
+        else
+        {
+            for (var i = 0; i < pixels.Length; i++)
+            {
+                pixels[i] = pixels[i] < 128 ? 0 : 255;
+            }
         }
         
         return pixels;
