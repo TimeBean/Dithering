783d2429f0c39838adfb0f62506bbb38c684eeb9
Alexander <alexanderklush8@gmail.com>
2026-02-28 18:39:51 +0300

    refactor: update console to new dither processor

--- DitherConsole/Program.cs
+++ DitherConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Dither.DitherStrategy;
 using Dither.Processors;
+using Dither.Processors.ErrorDiffusionProcessors;
 using Dither.Quantizers;
 using SkiaSharp;
 
@@ -29,6 +30,8 @@ namespace DitherConsole
         private static void Main()
         {
             using var input = File.OpenRead(@"Examples/Mirana.png");
+            //using var input = File.OpenRead(@"Examples/Vanko.jpg");
+
             using var originalBitmap = SKBitmap.Decode(input);
 
             var width = originalBitmap.Width;
@@ -49,7 +52,12 @@ namespace DitherConsole
             var processors = new (string Name, Func<IProcessor> Create)[]
             {
                 ("Base", () => new BaseProcessor(width, height, rowBytes, bpp)),
+                ("Atkinson", () => new AtkinsonProcessor(width, height, rowBytes, bpp)),
+                ("Burkes", () => new BurkesProcessor(width, height, rowBytes, bpp)),
                 ("Floyd-Steinberg", () => new FloydSteinbergProcessor(width, height, rowBytes, bpp)),
+                ("Jarvis-Judice-Ninke", () => new JarvisJudiceNinkeProcessor(width, height, rowBytes, bpp)),
+                ("Sierra_3_Row", () => new Sierra3Processor(width, height, rowBytes, bpp)),
+                ("Stucki", () => new StuckiProcessor(width, height, rowBytes, bpp)),
                 ("Bayer_2x2", () => new BayerProcessor(width, height, rowBytes, bpp, Constants.Bayer2)),
                 ("Bayer_4x4", () => new BayerProcessor(width, height, rowBytes, bpp, Constants.Bayer4)),
                 ("Bayer_8x8", () => new BayerProcessor(width, height, rowBytes, bpp, Constants.Bayer8)),
@@ -67,7 +75,8 @@ namespace DitherConsole
             };
 
             quantizers = quantizers
-                .Concat(palettes.Select(p => (Name: $"{p.Name} ({p.ColorCount})", Quantizer: (IQuantizer)new PaletteQuantizer(p.Data))))
+                .Concat(palettes.Select(p =>
+                    (Name: $"{p.Name} ({p.ColorCount})", Quantizer: (IQuantizer)new PaletteQuantizer(p.Data))))
                 .ToArray();
 
             using var compilationBitmap = new SKBitmap(width * quantizers.Length, height * processors.Length);
@@ -78,7 +87,7 @@ namespace DitherConsole
 
             var millis = 0L;
             var imagesCount = 0;
-            
+
             for (var pIndex = 0; pIndex < processors.Length; pIndex++)
             {
                 var perAlgorithmStopwatch = new Stopwatch();
@@ -113,8 +122,8 @@ namespace DitherConsole
                     DrawText(canvas, $"q: {quantizerText}", xPosition + leftOffset,
                         yPosition + topOffset + fontSize + 4 + fontSize);
                     DrawText(canvas, $"c: {dithered.GetColorCount()}", xPosition + leftOffset,
-                        yPosition + topOffset + fontSize + 4 + fontSize+ 4 + fontSize);
-                    
+                        yPosition + topOffset + fontSize + 4 + fontSize + 4 + fontSize);
+
                     perAlgorithmStopwatch.Start();
 
                     var fileName = $"Out/{processorDef.Name}_{quantizerDef.Name}.png";
@@ -131,7 +140,8 @@ namespace DitherConsole
 
             const string compilationFileName = "Out/!CompilationGrid.png";
             SaveBitmap(compilationBitmap, compilationFileName);
-            Console.WriteLine($"\nCompilation generated: {compilationFileName} ({millis} ms for {imagesCount} images i.e. {Math.Round(millis / (float)imagesCount, 2)} ms for image)");
+            Console.WriteLine(
+                $"\nCompilation generated: {compilationFileName} ({millis} ms for {imagesCount} images i.e. {Math.Round(millis / (float)imagesCount, 2)} ms for image)");
         }
 
         private static void DrawText(SKCanvas canvas, string text, int xPosition, int yPosition)
