bbcee66d905c59e2b0b2fcf6ee2547a5a6a11dd8
Alexander <alexanderklush8@gmail.com>
2026-03-01 14:56:27 +0300

    feat: reworked palette quantizer

---
+++ Dither/Quantizers/Palette/PaletteQuantizer.cs
@@ -0,0 +1,34 @@
+namespace Dither.Quantizers.Palette;
+
+public abstract class PaletteQuantizer : IQuantizer
+{
+    public float[,] Palette { get; }
+
+    protected PaletteQuantizer(float[,] palette)
+    {
+        Palette = palette;
+    }
+
+    public float[] Quantize(float[] pixels)
+    {
+        const int colorCount = 3;
+        var result = new float[pixels.Length];
+
+        for (var i = 0; i < pixels.Length; i += colorCount)
+        {
+            var nearestIndex = GetNearestColorIndex(
+                pixels[i],
+                pixels[i + 1],
+                pixels[i + 2]);
+
+            result[i]     = Palette[nearestIndex, 0];
+            result[i + 1] = Palette[nearestIndex, 1];
+            result[i + 2] = Palette[nearestIndex, 2];
+        }
+
+        return result;
+    }
+
+    protected abstract int GetNearestColorIndex(float r, float g, float b);
+
+}
